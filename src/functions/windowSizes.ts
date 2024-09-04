5059cffbaea268ba687a8181a1fc588689a44b15
Carolina <carolina.warntorp@outlook.com>
2024-09-04 21:59:27 +0200

    (bug) fix screen size fn

--- src/functions/windowSizes.ts
+++ src/functions/windowSizes.ts
@@ -6,27 +6,37 @@ export enum ScrSize {
   XXL = "XXL",
 }
 
+let innerWidth: number | null = null;
+
 export const windowSize = () => {
   const sBr = 576;
   const mBr = 768;
   const lBr = 992;
   const xlBr = 1200;
   const xxlBr = 1400;
-  if (typeof window !== undefined) {
-    if (window.innerWidth <= sBr) {
-      return ScrSize.S;
+
+  if (typeof window !== "undefined" && window !== null) {
+    innerWidth = window.innerWidth;
+  }
+
+  if (!innerWidth) {
+    console.warn("windowSize is being called outside of a browser environment");
+    return ScrSize.S;
+  }
+
+  if (innerWidth <= sBr) {
+    return ScrSize.S;
+  } else {
+    if (innerWidth <= mBr) {
+      return ScrSize.M;
     } else {
-      if (innerWidth <= mBr) {
-        return ScrSize.M;
+      if (innerWidth <= lBr) {
+        return ScrSize.L;
       } else {
-        if (innerWidth <= lBr) {
-          return ScrSize.L;
+        if (innerWidth <= xlBr) {
+          return ScrSize.XL;
         } else {
-          if (innerWidth <= xlBr) {
-            return ScrSize.XL;
-          } else {
-            return ScrSize.XXL;
-          }
+          return ScrSize.XXL;
         }
       }
     }
