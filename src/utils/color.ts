2289425abab7a5bd3d0c41e794f2c2841333d79d
Carolina <carolina.warntorp@outlook.com>
2024-09-04 12:18:23 +0200

    (build) add utils/color file

---
+++ src/utils/color.ts
@@ -0,0 +1,15 @@
+export const color = {
+  white: "white",
+  black: "black",
+  red: "darkred",
+  orange: "darkorange",
+  green: "darkgreen",
+
+  primaryText: "",
+
+  primaryBackground: "",
+  border: "",
+
+  pkmnCardYellow: "#ffcb05",
+  pkmnCardGrey: "",
+};
