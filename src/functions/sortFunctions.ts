3b3082dcc51d472b2a05f75178ea2bf527d59800
Carolina <carolina.warntorp@outlook.com>
2024-09-04 14:01:54 +0200

    (build) add sort functions

---
+++ src/functions/sortFunctions.ts
@@ -0,0 +1,63 @@
+import { ISavedCard } from "@/interfaces/interfaces";
+import { cardSum } from "./sumFunctions";
+
+// placeholder variable
+const cards: ISavedCard[] = [];
+
+interface ISortProps {
+  cards: ISavedCard[];
+  sortBy:
+    | "valueHighLow"
+    | "valueLowHigh"
+    | "nameAZ"
+    | "nameZA"
+    | "releaseOldNew"
+    | "releaseNewOld";
+}
+
+export const sortCards = ({ cards, sortBy }: ISortProps) => {
+  if (sortBy === "valueHighLow") {
+    cards.sort(
+      (a, b) =>
+        cardSum({ card: b.card, chosenType: b.type }) -
+        cardSum({ card: a.card, chosenType: a.type })
+    );
+  }
+  if (sortBy === "valueLowHigh") {
+    cards.sort(
+      (a, b) =>
+        cardSum({ card: a.card, chosenType: a.type }) -
+        cardSum({ card: b.card, chosenType: b.type })
+    );
+  }
+  if (sortBy === "nameAZ") {
+    cards.sort((a, b) =>
+      a.card.name
+        .toLocaleLowerCase()
+        .localeCompare(b.card.name.toLocaleLowerCase())
+    );
+  }
+  if (sortBy === "nameZA") {
+    cards.sort((a, b) =>
+      b.card.name
+        .toLocaleLowerCase()
+        .localeCompare(a.card.name.toLocaleLowerCase())
+    );
+  }
+  if (sortBy === "releaseOldNew") {
+    cards.sort(
+      (a, b) =>
+        parseInt(a.card.set.releaseDate.split("/")[0]) -
+        parseInt(b.card.set.releaseDate.split("/")[0])
+    );
+  }
+  if (sortBy === "releaseNewOld") {
+    cards.sort(
+      (a, b) =>
+        parseInt(b.card.set.releaseDate.split("/")[0]) -
+        parseInt(a.card.set.releaseDate.split("/")[0])
+    );
+  }
+
+  return cards;
+};
