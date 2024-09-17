62cd2b1fcd659a489c92f1305873e8470e852a36
Carolina <carolina.warntorp@outlook.com>
2024-09-17 15:27:08 +0200

    (bug) fix color function

--- src/functions/releaseYearFn.ts
+++ src/functions/releaseYearFn.ts
@@ -20,17 +20,33 @@ export const rarityCardColor = (release: string, rarity: string) => {
   const releaseDateNr = releaseDate.map((d) => parseInt(d, 10 || 0));
   const year = releaseDateNr[0];
   const month = releaseDateNr[1];
-  if (year >= 2023) {
-    if (month >= 0o3) {
-      if (rarity === "Hyper Rare") {
-        return color.pkmnCardGold;
-      } else {
-        return color.pkmnCardGrey;
-      }
+
+  if (year > 2023) {
+    console.log("higher than 2023", year);
+    if (rarity === "Hyper Rare") {
+      console.log("rare");
+      return color.pkmnCardGold;
     } else {
-      return color.pkmnCardYellow;
+      console.log("grey");
+      return color.pkmnCardGrey;
     }
   } else {
-    return color.pkmnCardYellow;
+    if (year == 2023) {
+      if (month >= 0o3) {
+        if (rarity === "Hyper Rare") {
+          console.log("rare");
+          return color.pkmnCardGold;
+        } else {
+          console.log(year);
+          return color.pkmnCardGrey;
+        }
+      } else {
+        console.log(year);
+        return color.pkmnCardYellow;
+      }
+    } else {
+      console.log(year);
+      return color.pkmnCardYellow;
+    }
   }
 };
